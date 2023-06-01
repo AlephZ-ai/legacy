1129424a545c14e937668a54eeabc3b920c36790
Aaron Stainback <aaron@alephz.ai>
2023-06-01 13:07:37 -0400

    Fix script of desktop-lite is not installed

--- .devcontainer/scripts/setup/devspace/desktop-lite.sh
+++ .devcontainer/scripts/setup/devspace/desktop-lite.sh
@@ -1,16 +1,23 @@
 #!/usr/bin/env bash
 # init
-  set -e
+set -e
 # desktop-lite fix for versions > 1.2.0
 # Find the version number by looking at the directory names
 NOVNC_VERSION=$(basename "$(find /usr/local/novnc -type d -name 'noVNC-*' | head -n 1)")
-# Construct the paths
-NOVNC_UTILS_PATH="/usr/local/novnc/${NOVNC_VERSION}/utils"
-LAUNCH_SCRIPT="${NOVNC_UTILS_PATH}/launch.sh"
-# Create the launch.sh script if it doesn't exist
-if [ ! -f "$LAUNCH_SCRIPT" ]; then
-    echo "${NOVNC_UTILS_PATH}/novnc_proxy \"\$@\"" | sudo tee "$LAUNCH_SCRIPT"
-    sudo chmod +x "$LAUNCH_SCRIPT"
+
+# Check if NOVNC_VERSION is not null
+if [[ -n "${NOVNC_VERSION}" ]]; then
+  # Construct the paths
+  NOVNC_UTILS_PATH="/usr/local/novnc/${NOVNC_VERSION}/utils"
+  LAUNCH_SCRIPT="${NOVNC_UTILS_PATH}/launch.sh"
+  # Create the launch.sh script if it doesn't exist
+  if [ ! -f "$LAUNCH_SCRIPT" ]; then
+      echo "${NOVNC_UTILS_PATH}/novnc_proxy \"\$@\"" | sudo tee "$LAUNCH_SCRIPT"
+      sudo chmod +x "$LAUNCH_SCRIPT"
+  else
+      echo "File $LAUNCH_SCRIPT already exists."
+  fi
 else
-    echo "File $LAUNCH_SCRIPT already exists."
+  echo "NOVNC_VERSION is null. Exiting."
+  exit 0
 fi
