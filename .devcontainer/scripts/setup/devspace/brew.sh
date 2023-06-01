6cfe30534189f814e955c2804cd0090aeb4d4ce9
Aaron Stainback <astainback@servicetitan.com>
2023-06-01 19:54:35 -0400

    Working on macos setup

--- .devcontainer/scripts/setup/devspace/brew.sh
+++ .devcontainer/scripts/setup/devspace/brew.sh
@@ -23,10 +23,10 @@
       #TODO: Fix
       if [ "$os" == "Linux" ]; then HOMEBREW_ACCEPT_EULA=Y brew install procps systemd; fi
     # These work on all brew platforms
-      HOMEBREW_ACCEPT_EULA=Y brew install sevenzip p7zip awk ca-certificates bash zsh oh-my-posh file-formula gnu-sed coreutils grep curl wget bzip2 less
+      HOMEBREW_ACCEPT_EULA=Y brew install sevenzip p7zip awk ca-certificates bash zsh oh-my-posh file-formula gnu-sed coreutils grep curl wget bzip2 less lesspipe
       HOMEBREW_ACCEPT_EULA=Y brew install zlib zlib-ng buf protobuf grpc dos2unix git git-lfs sigstore/tap/gitsign-credential-cache sigstore/tap/gitsign gh asdf
-      HOMEBREW_ACCEPT_EULA=Y brew install jq moreutils gcc make cmake cmake-docs llvm dotnet dotnet@6 mono go python@3.11 nss openssl@3 openssl@1.1 openssh age
-      HOMEBREW_ACCEPT_EULA=Y brew install nghttp2 mkcert shellcheck speedtest-cli mono-libgdiplus chezmoi sqlite sqlite-utils postgresql@15 azure-cli awscli
+      HOMEBREW_ACCEPT_EULA=Y brew install jq moreutils bash-completion@2 gcc make cmake cmake-docs llvm dotnet dotnet@6 mono go python@3.11 nss openssl@3 openssl@1.1
+      HOMEBREW_ACCEPT_EULA=Y brew install openssh age nghttp2 mkcert shellcheck speedtest-cli mono-libgdiplus chezmoi sqlite sqlite-utils postgresql@15 azure-cli awscli
       HOMEBREW_ACCEPT_EULA=Y brew install msodbcsql18 mssql-tools18 gedit
   # Upgrade all packages
     brew update
@@ -36,13 +36,16 @@
       sudo chsh "$USERNAME" -s "$(which zsh)"
     fi
 
-    brew link --force --overwrite postgresql@15 openssl@3
-    # shellcheck disable=SC2016
-    updaterc "export PATH=\"$HOMEBREW_PREFIX/opt/python/libexec/bin:\$PATH\""
+    brew link --force --overwrite bash-completion@2 openssl@3 postgresql@15
+    # shellcheck source=/dev/null
+    [[ -r "$HOMEBREW_PREFIX/etc/profile.d/bash_completion.sh" ]] && source "$HOMEBREW_PREFIX/etc/profile.d/bash_completion.sh"
+    rcLine="[[ -r \"$HOMEBREW_PREFIX//etc/profile.d/bash_completion.sh\" ]] && source \"$HOMEBREW_PREFIX/etc/profile.d/bash_completion.sh\""
+    grep -qxF "$rcLine" "$HOME/.bashrc" || echo "$rcLine" >> "$HOME/.bashrc"
     updaterc "source \"$HOMEBREW_PREFIX/opt/asdf/libexec/asdf.sh\""
     updaterc "export MONO_GAC_PREFIX=\"$HOMEBREW_PREFIX\""
-    # shellcheck disable=SC2016
+    updaterc "export PATH=\"$HOMEBREW_PREFIX/opt/python/libexec/bin:\$PATH\""
+    updaterc "export PATH=\"$HOMEBREW_PREFIX/opt/coreutils/libexec/gnubin:\$PATH\""
     updaterc "export PATH=\"$HOMEBREW_PREFIX/opt/gnu-sed/libexec/gnubin:\$PATH\""
   # Run Homebrew cleanup and doctor to check for errors
     brew cleanup
-    brew doctor
+    brew doctor || true
--- .devcontainer/scripts/setup/devspace/edge-default.sh
+++ .devcontainer/scripts/setup/devspace/edge-default.sh
@@ -3,18 +3,21 @@
   set -e
   # shellcheck source=/dev/null
   source "$HOME/.bashrc"
+  os=$(uname -s)
 # Make Edge the default browser if installed
-  browser='/usr/bin/microsoft-edge-stable'
-  cmds=("alias xdg-open=$browser" "export BROWSER=$browser")
-  seds=("s:^alias xdg-open=.*$:alias xdg-open=$browser:" "s:^export BROWSER=.*$:export BROWSER=$browser:")
-  files=("$HOME/.bashrc" "$HOME/.zshrc")
-  # shellcheck disable=SC2068
-  for i in ${!cmds[@]}; do
-    cmd="${cmds[$i]}"
-    sed="${seds[$i]}"
-    eval "$cmd"
-    for file in "${files[@]}"; do
-      sed -i "$sed" "$file"
-      grep -qF "$cmd" "$file" || echo "$cmd" >> "$file"
+  if [ "$os" == "Linux" ]; then
+    browser='/usr/bin/microsoft-edge-stable'
+    cmds=("alias xdg-open=$browser" "export BROWSER=$browser")
+    seds=("s:^alias xdg-open=.*$:alias xdg-open=$browser:" "s:^export BROWSER=.*$:export BROWSER=$browser:")
+    files=("$HOME/.bashrc" "$HOME/.zshrc")
+    # shellcheck disable=SC2068
+    for i in ${!cmds[@]}; do
+      cmd="${cmds[$i]}"
+      sed="${seds[$i]}"
+      eval "$cmd"
+      for file in "${files[@]}"; do
+        sed -i "$sed" "$file"
+        grep -qF "$cmd" "$file" || echo "$cmd" >> "$file"
+      done
     done
-  done
+  fi
