c3a8cdb4706e76d8fcf4b9a7f6a02071423510c4
Aaron Stainback <aaron@alephz.ai>
2023-06-02 01:03:53 -0400

    WSLg

--- .devcontainer/scripts/setup/environment.cmd
+++ .devcontainer/scripts/setup/environment.cmd
@@ -10,6 +10,19 @@ popd
 set "DEVCONTAINER_FEATURES_PROJECT_ROOT=%projectRoot%"
 set "DEVCONTAINER_FEATURES_SOURCE_ROOT=%DEVCONTAINER_FEATURES_PROJECT_ROOT%\src"
 set "DEVCONTAINER_SCRIPTS_ROOT=%DEVCONTAINER_FEATURES_PROJECT_ROOT%\.devcontainer\scripts"
+for /f "delims=" %%i in ('wsl echo $DISPLAY') do set "wd=%%i"
+if errorlevel 1 (
+    echo WSL not found
+    exit /b
+)
+
+set "LIB_WSL=/usr/lib/wsl"
+if not "%wd%"=="" (
+    set "MNT_WSLG=/mnt/wslg"
+    set "X11_SOCKET=/tmp/.X11-unix"
+) else (
+    echo WSLg not found
+)
 
 set
 endlocal
