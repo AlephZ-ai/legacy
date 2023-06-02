e1e9b13273da55d7ef78660513bfc53412cee853
Aaron Stainback <aaron@alephz.ai>
2023-06-02 07:58:40 -0400

    Working on vscode user

--- .devcontainer/scripts/docker/create-user.sh
+++ .devcontainer/scripts/docker/create-user.sh
@@ -5,13 +5,17 @@ USER_UID="$2"
 USER_GID="$3"
 
 if id "$USERNAME"; then 
-    echo "User $USERNAME already exists"
+  groupmod --gid "$USER_GID" "$USERNAME"
+  usermod --uid "$USER_UID" --gid "$USER_GID" "$USERNAME"
+  chown -R "$USER_UID":"$USER_GID" "/home/$USERNAME"
 else
-    echo "Creating user $USERNAME"
-    groupadd --gid $USER_GID $USERNAME
-    useradd --uid $USER_UID --gid $USER_GID -m $USERNAME
-    apt-get update
-    apt-get install -y sudo
-    echo $USERNAME ALL=\(root\) NOPASSWD:ALL > /etc/sudoers.d/$USERNAME
-    chmod 0440 /etc/sudoers.d/$USERNAME
+  echo "Creating user $USERNAME"
+  groupadd --gid "$USER_GID" "$USERNAME"
+  useradd --uid "$USER_UID" --gid "$USER_GID" -m "$USERNAME"
+  apt-get update
+  apt-get install -y sudo
+  line="$USERNAME ALL=\(root\) NOPASSWD:ALL"
+  file="/etc/sudoers.d/$USERNAME"
+  grep -qxF "$line" "$file" || echo "$line" >> "$file"
+  chmod 0440 "/etc/sudoers.d/$USERNAME"
 fi
