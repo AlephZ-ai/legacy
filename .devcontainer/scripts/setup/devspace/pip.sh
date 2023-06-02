35cd5fc6bb75052c01397e1fc2413d0e672d02d1
Aaron Stainback <aaron@alephz.ai>
2023-06-02 09:07:04 -0400

    Adding more feature

--- .devcontainer/scripts/setup/devspace/pip.sh
+++ .devcontainer/scripts/setup/devspace/pip.sh
@@ -6,6 +6,9 @@
 # Setup pip
   python -m ensurepip --upgrade
   python -m pip install --upgrade pip
-  pip install --use-pep517 pip-review
-  pip install moreutils
+  pip install --upgrade --use-pep517 pip-review
+  pip install --upgrade py moreutils setuptools wheel pylint autopep8 black mypy flake8
+  pip install --upgrade pytest pytest-cov pytest-xdist pytest-html pytest-mock pytest-randomly pytest-sugar pytest-timeout pytest-asyncio pytest-asyncio-threading pytest-asyncio-network-simulator
+  pip install --upgrade jupyter jupyterlab jupyterlab-git jupyterlab-lsp jupyterlab_code_formatter jupyterlab_execute_time jupyterlab-kernelspy jupyterlab-system-monitor jupyterlab-topbar-extension
+  pip install --upgrade numpy pandas scipy matplotlib seaborn scikit-learn scikit-image cntk tensorflow keras torch torchvision transformers spacy nltk gensim opencv-python
   pip-review --auto
--- .devcontainer/scripts/setup/windows/scoop.ps1
+++ .devcontainer/scripts/setup/windows/scoop.ps1
@@ -9,7 +9,7 @@ do {
         scoop install --global touch patch
         scoop install --global gawk cacert file sed coreutils grep curl wget bzip2 less
         scoop install --global zlib buf protobuf grpc-tools dos2unix gitsign gh
-        scoop install --global jq gcc make cmake llvm dotnet-nightly dotnet-sdk-preview dotnet-sdk dotnet-sdk-lts mono go python openssl openssh age 
+        scoop install --global jq gcc make cmake llvm dotnet-nightly dotnet-sdk-preview dotnet-sdk dotnet-sdk-lts mono go rust python openssl openssh age 
         scoop install --global mkcert shellcheck speedtest-cli chezmoi sqlite postgresql azure-cli aws
         scoop install --global gedit kubeadm kubectl helm minikube kind k3d argo-cd k9s kustomize skaffold vcluster
         scoop install --global terraform
