c681b48a32b922c0795002bda1f9bbbd99bb0057
Aaron Stainback <astainback@servicetitan.com>
2023-06-03 18:40:08 -0400

    Working on mac setup

--- .devcontainer/scripts/setup/macos/brew.sh
+++ .devcontainer/scripts/setup/macos/brew.sh
@@ -15,6 +15,11 @@
   brew upgrade --cask
 # Install xquartz
   brew install --cask microsoft-openjdk iterm2 microsoft-edge xquartz
+# Setup post hombrew packages
+  sudo ln -sfn "$HOMEBREW_PREFIX/opt/openjdk/libexec/openjdk.jdk" /Library/Java/JavaVirtualMachines/openjdk.jdk
+  sudo ln -sfn "$HOMEBREW_PREFIX/opt/openjdk@8/libexec/openjdk.jdk" /Library/Java/JavaVirtualMachines/openjdk-8.jdk
+  sudo ln -sfn "$HOMEBREW_PREFIX/opt/openjdk@11/libexec/openjdk.jdk" /Library/Java/JavaVirtualMachines/openjdk-11.jdk
+  sudo ln -sfn "$HOMEBREW_PREFIX/opt/openjdk@17/libexec/openjdk.jdk" /Library/Java/JavaVirtualMachines/openjdk-17.jdk
 # Run Homebrew cleanup and doctor to check for errors
   brew cleanup
   brew doctor || true
