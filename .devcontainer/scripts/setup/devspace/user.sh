a75f4add9d210e8e6b90b0d619310e747322f942
Aaron Stainback <aaron@alephz.ai>
2023-06-03 16:59:57 -0400

    Working on sudoers

--- .devcontainer/scripts/setup/devspace/user.sh
+++ .devcontainer/scripts/setup/devspace/user.sh
@@ -4,8 +4,11 @@
   # shellcheck source=/dev/null
   source /etc/bash.bashrc
   USERNAME=${USERNAME:-}
+  os=$(uname -s)
 # Disable needing password for sudo
-  line="$USERNAME ALL=(ALL:ALL) NOPASSWD: ALL"
-  file="/etc/sudoers.d/$USERNAME"
-  grep -qxF "$line" "$file" || echo "$line" >> "$file"
-  chmod 440 "$file"
+  if [ "$os" == "Linux" ]; then
+    line="$USERNAME ALL=(ALL:ALL) NOPASSWD: ALL"
+    file="/etc/sudoers.d/$USERNAME"
+    grep -qxF "$line" "$file" || echo "$line" >> "$file"
+    chmod 440 "$file"
+  fi
