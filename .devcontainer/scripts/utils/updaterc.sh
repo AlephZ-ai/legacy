cc169fd5c0322959bc973390253f8c7fa0e3a297
Aaron Stainback <astainback@servicetitan.com>
2023-06-04 05:16:34 -0400

    Update RC

--- .devcontainer/scripts/utils/updaterc.sh
+++ .devcontainer/scripts/utils/updaterc.sh
@@ -1,4 +1,5 @@
 # shellcheck shell=bash
+set -ex
 # Splits a string by semicolon
 split_string() {
   IFS=';' read -r -a arr <<<"$1"
@@ -6,26 +7,25 @@ split_string() {
 }
 
 cmd="$1"
-# Check if $1 starts with sudo and set $2 as "sudo" if it's unset or empty
-if [[ $cmd == sudo* && -z "$2" ]]; then
-  option="sudo"
-else
-  option="$2"
-fi
+option="$2"
+
+# Split the command into an array
+IFS=' ' read -r -a cmd_parts <<<"$cmd"
+
+# Store the first part of the command in an environment variable
+CMD_FIRST_PART="${cmd_parts[0]}"
 
 if [[ "$option" == "sudo" ]]; then
   rcs=("/etc/bash.bashrc" "/etc/zsh/zshrc")
+  [[ $cmd != sudo* ]] && cmd="sudo $cmd"
 elif [[ -z "$option" ]]; then
   rcs=("$HOME/.bashrc" "$HOME/.zshrc")
 else
   IFS=';' read -r -a rcs <<<"$(split_string "$option")"
 fi
 
-# Split the command into an array
-IFS=' ' read -r -a cmd_parts <<<"$cmd"
-
 # Check if the first part of the command is 'sudo'
-if [[ "${cmd_parts[0]}" == 'sudo' ]]; then
+if [ "$CMD_FIRST_PART" = 'sudo' ]; then
   # If it is, run the command with sudo
   sudo "${cmd_parts[@]:1}"
 else
@@ -37,7 +37,7 @@ printf 'Updating: %s\n' "${rcs[@]}"
 for rc in "${rcs[@]}"; do
   if [[ "$(cat "$rc")" != *"$cmd"* ]]; then
     # Check again if the original command starts with sudo
-    if [[ "${cmd_parts[0]}" == 'sudo' ]]; then
+    if [ "$CMD_FIRST_PART" = 'sudo' ]; then
       # If it is, append the command to the file with sudo
       echo -e "$cmd" | sudo tee -a "$rc" >/dev/null
     else
