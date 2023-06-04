542bf764a28eaf8460c50c807bb45e80298f69ba
Aaron Stainback <astainback@servicetitan.com>
2023-06-03 23:35:38 -0400

    Update pip

--- .devcontainer/scripts/setup/devspace/pip.sh
+++ .devcontainer/scripts/setup/devspace/pip.sh
@@ -7,13 +7,13 @@
     python -m ensurepip --upgrade
     python -m pip install --no-input --upgrade pip setuptools wheel
     while ! (
-      pip install --no-input --upgrade pygobject pycairo platformdirs astroid dill isort mccabe ipykernel ipython-genutils pygments \
-        flake8 pytest pytest-sugar pytest-cov pytest-xdist pytest-xprocess pytest-mock pytest-benchmark \
-        autopep8 opencv-python aiosqlite absl-py astunparse flatbuffers gast google-pasta grpcio h5py jax \
-        keras libclang numpy opt-einsum protobuf blis catalogue confection cymem murmurhash preshed \
-        black yapf pydantic jinja2 langcodes murmurhash pathy filelock huggingface-hub imageio lazy_loader \
-        networkx pillow tensorboard wrapt py moreutils pylint mypy pandas scipy matplotlib seaborn \
-        scikit-learn scikit-image tensorflow torch torchvision transformers spacy nltk gensim \
+      pip install --no-input --upgrade pip setuptools wheel pygobject pycairo platformdirs astroid dill isort \
+        mccabe ipykernel ipython-genutils pygments flake8 pytest pytest-sugar pytest-cov pytest-xdist \
+        pytest-xprocess pytest-mock pytest-benchmark autopep8 opencv-python aiosqlite absl-py astunparse \
+        flatbuffers gast google-pasta grpcio h5py jax keras libclang numpy opt-einsum protobuf blis catalogue \
+        confection cymem murmurhash preshed black yapf pydantic jinja2 langcodes murmurhash pathy filelock \
+        huggingface-hub imageio lazy_loader networkx pillow tensorboard wrapt py moreutils pylint mypy pandas scipy \
+        matplotlib seaborn scikit-learn scikit-image tensorflow torch torchvision transformers spacy nltk gensim \
         jupyter-client jupyter-core jupyter jupyter-lsp jupyterlab jupyterlab-fonts jupyterlab-git jupyterlab-markup \
         jupyterlab_widgets jupyterlab-commands jupyterlab_code_formatter jupyterlab-black jupyterlab-requirements \
         jupyterlab-sparksql jupyterlab-drawio jupyterlab-powerpoint jupyterlab-github jupyterlab-flake8 jupyterlab-lsp \
--- .devcontainer/scripts/setup/windows/pip.ps1
+++ .devcontainer/scripts/setup/windows/pip.ps1
@@ -1,13 +1,13 @@
 Write-Host "setup/windows/pip.ps1"
 python -m ensurepip --upgrade | Write-Host
 python -m pip install --upgrade pip setuptools wheel | Write-Host
-pip install --upgrade pygobject platformdirs astroid dill isort mccabe ipykernel ipython-genutils pygments \
-  flake8 pytest pytest-sugar pytest-cov pytest-xdist pytest-xprocess pytest-mock pytest-benchmark \
-  autopep8 opencv-python aiosqlite absl-py astunparse flatbuffers gast google-pasta grpcio h5py jax \
-  keras libclang numpy opt-einsum protobuf blis catalogue confection cymem murmurhash preshed \
-  black yapf pydantic jinja2 langcodes murmurhash pathy filelock huggingface-hub imageio lazy_loader \
-  networkx pillow tensorboard wrapt py moreutils pylint mypy pandas scipy matplotlib seaborn \
-  scikit-learn scikit-image tensorflow torch torchvision transformers spacy nltk gensim \
+pip install --no-input --upgrade pip setuptools wheel pygobject pycairo platformdirs astroid dill isort \
+  mccabe ipykernel ipython-genutils pygments flake8 pytest pytest-sugar pytest-cov pytest-xdist \
+  pytest-xprocess pytest-mock pytest-benchmark autopep8 opencv-python aiosqlite absl-py astunparse \
+  flatbuffers gast google-pasta grpcio h5py jax keras libclang numpy opt-einsum protobuf blis catalogue \
+  confection cymem murmurhash preshed black yapf pydantic jinja2 langcodes murmurhash pathy filelock \
+  huggingface-hub imageio lazy_loader networkx pillow tensorboard wrapt py moreutils pylint mypy pandas scipy \
+  matplotlib seaborn scikit-learn scikit-image tensorflow torch torchvision transformers spacy nltk gensim \
   jupyter-client jupyter-core jupyter jupyter-lsp jupyterlab jupyterlab-fonts jupyterlab-git jupyterlab-markup \
   jupyterlab_widgets jupyterlab-commands jupyterlab_code_formatter jupyterlab-black jupyterlab-requirements \
   jupyterlab-sparksql jupyterlab-drawio jupyterlab-powerpoint jupyterlab-github jupyterlab-flake8 jupyterlab-lsp \
