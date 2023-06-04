847f9e11c899b50d6d1666ab8f8742d16b4f48ad
Aaron Stainback <astainback@servicetitan.com>
2023-06-03 21:15:27 -0400

    Working on mac

--- .devcontainer/scripts/uninstall/brew/brews.sh
+++ .devcontainer/scripts/uninstall/brew/brews.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+brew uninstall --force --ignore-dependencies pycairo
 "$DEVCONTAINER_SCRIPTS_ROOT/uninstall/pip/packages.sh"
 "$DEVCONTAINER_SCRIPTS_ROOT/uninstall/pip/package-cache.sh"
 while [[ $(brew list | wc -l) -ne 0 ]]; do
