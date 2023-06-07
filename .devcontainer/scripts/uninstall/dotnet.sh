0bffd5176e616490d18013e2a27f4defe094999c
Aaron Stainback <astainback@servicetitan.com>
2023-06-07 00:26:53 -0400

    Working on dotnet

--- .devcontainer/scripts/uninstall/dotnet.sh
+++ .devcontainer/scripts/uninstall/dotnet.sh
@@ -5,6 +5,7 @@ if command -v dotnet --version --version >/dev/null 2>&1; then
     dotnet workload uninstall "$workload"
   done
 
+  dotnet tool list --tool-path "$HOME/.dotnet/tools/preview" | awk 'NR>2 {print $1}' | xargs -I {} -n1 dotnet tool uninstall --tool-path "$HOME/.dotnet/tools/preview" "{}"
   dotnet tool list -g | awk 'NR>2 {print $1}' | xargs -I {} -n1 dotnet tool uninstall -g "{}"
 fi
 
