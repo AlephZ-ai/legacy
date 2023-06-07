e9f403842115637a068b8714f2a9cbcf8945214b
Aaron Stainback <astainback@servicetitan.com>
2023-06-07 07:21:55 -0400

    ADA

--- .devcontainer/scripts/uninstall/dotnet.sh
+++ .devcontainer/scripts/uninstall/dotnet.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 set -e
+if [ -e "$HOME/.dotnet/tools/preview" ]; then
+  dotnet tool list --tool-path "$HOME/.dotnet/tools/preview" | awk 'NR>2 {print $1}' | xargs -I {} -n1 dotnet tool uninstall --tool-path "$HOME/.dotnet/tools/preview" "{}"
+fi
+
+dotnet tool list -g | awk 'NR>2 {print $1}' | xargs -n1 dotnet tool uninstall -g
 if command -v dotnet --version >/dev/null 2>&1; then
   list=$(dotnet workload list | awk 'NR>2 {print $1}')
   if [ -n "$list" ]; then
@@ -7,12 +12,6 @@ if command -v dotnet --version >/dev/null 2>&1; then
       dotnet workload uninstall "$workload" &>/dev/null || true
     done
   fi
-
-  if [ -e "$HOME/.dotnet/tools/preview" ]; then
-    dotnet tool list --tool-path "$HOME/.dotnet/tools/preview" | awk 'NR>2 {print $1}' | xargs -I {} -n1 dotnet tool uninstall --tool-path "$HOME/.dotnet/tools/preview" "{}"
-  fi
-
-  dotnet tool list -g | awk 'NR>2 {print $1}' | xargs -n1 dotnet tool uninstall -g
 fi
 
 rm -rf "$HOME/.dotnet"
