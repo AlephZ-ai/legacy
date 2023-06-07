9b425be73e18031a47f00ba2a300d1269026f07c
Aaron Stainback <astainback@servicetitan.com>
2023-06-07 06:58:10 -0400

    AA

--- .devcontainer/scripts/setup/devspace/pwsh.sh
+++ .devcontainer/scripts/setup/devspace/pwsh.sh
@@ -5,10 +5,10 @@ set -e
 # shellcheck source=/dev/null
 source "$HOME/.bashrc"
 # Setup pwsh modules
-pwsh_modules=('Pester' 'Set-PsEnv' 'posh-docker' 'posh-git' 'lazy-posh-git' 'Az' 'AAWS.Tools.Installer' 'PowerCLI' 'PSReadLine'
+pwsh_modules=('Pester' 'Set-PsEnv' 'posh-docker' 'posh-git' 'lazy-posh-git' 'Az' 'AWS.Tools.Installer' 'PowerCLI' 'PSReadLine'
   'SqlServer' 'ImportExcel' 'PSScriptAnalyzer' 'dbatools')
 # shellcheck disable=SC2016
-pwsh_update='if (!(Test-Path $PROFILE)) { New-Item -ItemType File -Path $PROFILE -Force; }; Install-Module PowerShellGet -ErrorAction Stop -Force -SkipPublisherCheck -AllowClobber; Install-Module xPackageManagement -ErrorAction Stop -Force -SkipPublisherCheck -AllowClobber; Update-Module; Install-Module PowerShellGet -ErrorAction Stop -Force -SkipPublisherCheck -AllowClobber -AllowPrerelease; Set-Alias -Name awk -Value gawk'
+pwsh_update='if (!(Test-Path $PROFILE)) { New-Item -ItemType File -Path $PROFILE -Force; }; Install-Module PowerShellGet -ErrorAction Stop -Force -SkipPublisherCheck -AllowClobber; Install-Module PackageManagement -ErrorAction Stop -Force -SkipPublisherCheck -AllowClobber; Update-Module; Install-Module PowerShellGet -ErrorAction Stop -Force -SkipPublisherCheck -AllowClobber -AllowPrerelease; Set-Alias -Name awk -Value gawk'
 # shellcheck disable=SC2016
 pwsh_install_module='Install-Module $module -ErrorAction Stop -Force -SkipPublisherCheck -AllowClobber;'
 # shellcheck disable=SC2034
