1f75ce6e3d4bc28428fc53437adcf305e9d35707
Aaron Stainback <astainback@servicetitan.com>
2023-06-10 03:37:53 -0400

    WOPL10k

--- .devcontainer/scripts/setup/devspace/zsh/powerlevel10k.sh
+++ .devcontainer/scripts/setup/devspace/zsh/powerlevel10k.sh
@@ -3,20 +3,6 @@
 # shellcheck source=/dev/null
 # init
 set -euo pipefail
-echo -e "Installing powerlevel10k"
-themes="${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes"
-plpath="$themes/powerlevel10k"
-if [ ! -d "$plpath" ]; then
-  mkdir -p "$themes"
-  git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "$plpath" &>/dev/null || true
-fi
-
-if [ "$FAST_LEVEL" -eq 0 ]; then
-  pushd "$plpath"
-  git pull
-  popd
-fi
-
 echo -e "RECOMMENDED: Install Meslo Nerd Font patched for Powerlevel10k"
 echo -e "https://github.com/romkatv/powerlevel10k#meslo-nerd-font-patched-for-powerlevel10k"
 # TODO: Finish setting up
