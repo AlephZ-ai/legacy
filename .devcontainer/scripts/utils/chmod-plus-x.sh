031ef6e02d4f523923ceded39b624a661bedbc1a
Aaron Stainback <astainback@servicetitan.com>
2023-06-14 14:04:41 -0400

    chmod +x

--- .devcontainer/scripts/utils/chmod-plus-x.sh
+++ .devcontainer/scripts/utils/chmod-plus-x.sh
@@ -8,3 +8,5 @@ find "$DEVCONTAINER_PROJECT_ROOT/scripts" -type f -iname "*.sh" -exec git update
 find "$DEVCONTAINER_FEATURES_PROJECT_ROOT/src" -type f -iname "*.sh" -exec git update-index --add --chmod=+x "{}" \;
 find "$DEVCONTAINER_FEATURES_PROJECT_ROOT/test" -type f -iname "*.sh" -exec git update-index --add --chmod=+x "{}" \;
 find "$DEVCONTAINER_FEATURES_PROJECT_ROOT" -type f -iname "*.sh" -exec chmod +x "{}" \;
+find "$DEVCONTAINER_FEATURES_PROJECT_ROOT" -type f -iname "*.ps1" -exec git update-index --add --chmod=+x "{}" \;
+find "$DEVCONTAINER_FEATURES_PROJECT_ROOT" -type f -iname "*.ps1" -exec chmod +x "{}" \;
